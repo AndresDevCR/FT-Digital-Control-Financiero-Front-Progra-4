897179ba612e0e446f4357e80a9da3417d60ab2e
Andrés Vargas <andres.vargas@fusiontech.pro>
2023-04-01 17:53:45 -0600

    nav changes

--- src/layouts/dashboard/nav/NavVertical.js
+++ src/layouts/dashboard/nav/NavVertical.js
@@ -14,7 +14,7 @@ import Scrollbar from '../../../components/scrollbar';
 import { NavSectionVertical } from '../../../components/nav-section';
 //
 import navConfig from './config-navigation';
-import NavDocs from './NavDocs';
+// import NavDocs from './NavDocs';
 import NavAccount from './NavAccount';
 import NavToggleButton from './NavToggleButton';
 
@@ -66,7 +66,7 @@ export default function NavVertical({ openNav, onCloseNav }) {
 
       <Box sx={{ flexGrow: 1 }} />
 
-      <NavDocs />
+      {/* <NavDocs /> */}
     </Scrollbar>
   );
 
