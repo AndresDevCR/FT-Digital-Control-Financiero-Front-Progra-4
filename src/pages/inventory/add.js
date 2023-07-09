18463fe32bce56e68a44f9abec9255a49e02a870
Andrés Vargas <120044405+AndresVargasFT@users.noreply.github.com>
2023-07-08 23:38:42 -0600

    prod changes

--- src/pages/inventory/add.js
+++ src/pages/inventory/add.js
@@ -1,15 +1,11 @@
 import InventoryForm from "../../components/forms/inventory/InventoryForm";
 import DashboardLayout from '../../layouts/dashboard';
-// components
-
 
 
 add.getLayout = (page) => <DashboardLayout>{page}</DashboardLayout>;
 
-export default function add () {
-    
-    
-    return (
-        <InventoryForm/>
-    )
+export default function add() {
+  return (
+    <InventoryForm />
+  );
 }
