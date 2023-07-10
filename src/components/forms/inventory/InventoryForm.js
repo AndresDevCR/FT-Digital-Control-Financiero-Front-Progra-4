3ee451be2e833960624f333826144919af632a36
Ricardo1697 <rvr161997@gmail.com>
2023-07-09 21:47:23 -0600

    agregar validaciones en inventario

--- src/components/forms/inventory/InventoryForm.js
+++ src/components/forms/inventory/InventoryForm.js
@@ -1,115 +1,182 @@
-// next
-import Head from 'next/head';
+import React, { useContext } from 'react';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
 import { Container, Typography, Box, Grid, TextField, Button } from '@mui/material';
-
-// layouts
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-// components
-import { useSettingsContext } from '../../settings';
+import axios from 'axios';
+import { useRouter } from 'next/router';
+import { AuthContext } from '../../../auth/JwtContext';
 
-// ----------------------------------------------------------------------
-
-
-// ----------------------------------------------------------------------
+const validationSchema = Yup.object().shape({
+  productName: Yup.string()
+    .required('Nombre del producto/servicio es requerido')
+    .max(30, 'El nombre debe tener como máximo 30 caracteres'),
+  availableQuantity: Yup.number()
+    .required('Cantidad disponible es requerida')
+    .integer('Solo se permiten números enteros')
+    .max(999999, 'La cantidad debe tener como máximo 999,999'),
+  description: Yup.string()
+    .required('Descripción es requerida')
+    .max(200, 'La descripción debe tener como máximo 200 caracteres'),
+  entryDate: Yup.date().required('Fecha de ingreso es requerida'),
+});
 
 export default function InventoryForm() {
-    const { themeStretch } = useSettingsContext();
+  const { accessToken } = useContext(AuthContext);
+  const router = useRouter();
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        toast.success('Agregado correctamente al inventario');
-    };
+  const handleSubmit = async (values) => {
+    try {
+      await axios.post('https://control-financiero.herokuapp.com/api/v1/inventory', values, {
+        headers: {
+          Authorization: `Bearer ${accessToken}`, // Incluye el token de autenticación en el encabezado
+        },
+      });
+      toast.success('Agregado correctamente al inventario');
+      router.push('/inventory'); // Redireccionar a la lista de inventario
+    } catch (error) {
+      toast.error('Error al agregar al inventario');
+    }
+  };
 
-    return (
-        <>
-            <Head>
-                <title> Agregar Inventario | FT Control Financiero</title>
-            </Head>
+  const formik = useFormik({
+    initialValues: {
+      productName: '',
+      availableQuantity: 1,
+      description: '',
+      entryDate: '',
+    },
+    validationSchema,
+    onSubmit: handleSubmit,
+  });
 
-            <Container maxWidth={themeStretch ? false : 'xl'}>
-                <Typography variant="h3" component="h1" paragraph>
-                    Agregar Inventario
-                </Typography>
-            </Container>
+  const handleInputChange = (event) => {
+    if (event.target.name === 'productName' && event.target.value.length >= 30) {
+      toast.info('Se ha alcanzado el límite de caracteres permitidos');
+    }
 
-            <ToastContainer />
+    if (event.target.name === 'availableQuantity' && !/^\d+$/.test(event.target.value)) {
+      toast.error('Solo se permiten números en el campo de Cantidad disponible');
+    }
 
-            {/* formulario de agregar Inventario */}
-            <Container maxWidth={themeStretch ? false : 'xl'}>
-                <Box
-                    component="form"
-                    noValidate
-                    autoComplete="off"
-                    sx={{ mt: 3 }}
-                    onSubmit={handleSubmit}
-                >
-                    <Grid container spacing={3}>
-                        <Grid item xs={12} md={6}>
-                            <TextField
-                                fullWidth
-                                label="Nombre del producto/servicio"
-                                name="inventoryName"
-                            // value={name}
-                            // onChange={handleChange}
-                            />
-                        </Grid>
+    if (event.target.name === 'availableQuantity' && event.target.value.length >= 7) {
+      event.target.value = event.target.value.slice(0, 6); // Limitar a 6 dígitos
+      toast.info('Se ha alcanzado el límite de numeros permitidos ');
+    }
 
-                        <Grid item xs={12} md={6}>
-                            <TextField
-                                fullWidth
-                                label="Cantidad disponible"
-                                name="inventoryQty"
-                            // value={name}
-                            // onChange={handleChange}
-                            />
-                        </Grid>
+    if (event.target.name === 'description' && event.target.value.length >= 200) {
+      toast.info('Se ha alcanzado el límite de caracteres permitidos para la descripción');
+    }
 
-                        <Grid item xs={12} md={12}>
-                            <TextField
-                                fullWidth
-                                label="Descripcion"
-                                name="inventoryDescription"
-                                multiline
-                                minRows={5}
-                                maxRows={10}
-                            // value={address}
-                            // onChange={handleChange}
-                            />
-                        </Grid>
+    formik.handleChange(event);
+  };
 
-                        <Grid item xs={12} md={6}>
-                            <TextField
-                                fullWidth
-                                label="Fecha de ingreso"
-                                name="inventoryDate"
-                            // value={date}
-                            // onChange={handleChange}
-                            />
+  return (
+    <>
+      <Container>
+        <Typography variant="h3" component="h1" paragraph>
+          Agregar Inventario
+        </Typography>
+      </Container>
 
-                        </Grid>
+      <ToastContainer />
 
-                        {/* boton del formulario */}
-                        <Grid item xs={12} md={12}>
-                            <Button
-                                fullWidth
-                                size="large"
-                                type="submit"
-                                variant="contained"
-                                onClick={handleSubmit}
-                            >
-                                Guardar
-                            </Button>
-                        </Grid>
+      <Container>
+        <Box
+          component="form"
+          noValidate
+          autoComplete="off"
+          sx={{ mt: 3 }}
+          onSubmit={formik.handleSubmit}
+        >
+          <Grid container spacing={3}>
+            <Grid item xs={12} md={6}>
+              <TextField
+                fullWidth
+                label="Nombre del producto/servicio"
+                name="productName"
+                value={formik.values.productName}
+                onChange={handleInputChange}
+                error={formik.touched.productName && formik.errors.productName}
+                helperText={formik.touched.productName && formik.errors.productName}
+                inputProps={{
+                  maxLength: 30,
+                }}
+              />
+            </Grid>
 
+            <Grid item xs={12} md={6}>
+              <TextField
+                fullWidth
+                label="Cantidad disponible"
+                name="availableQuantity"
+                type="number"
+                value={formik.values.availableQuantity}
+                onChange={handleInputChange}
+                error={formik.touched.availableQuantity && formik.errors.availableQuantity}
+                helperText={formik.touched.availableQuantity && formik.errors.availableQuantity}
+                inputProps={{
+                  max: 999999,
+                  maxLength: 6,
+                }}
+              />
+            </Grid>
 
-                        {/* fin de formulario */}
-                    </Grid>
-                </Box>
-            </Container>
+            <Grid item xs={12} md={12}>
+              <TextField
+                fullWidth
+                label="Descripción"
+                name="description"
+                multiline
+                minRows={5}
+                maxRows={10}
+                value={formik.values.description}
+                onChange={handleInputChange}
+                error={formik.touched.description && formik.errors.description}
+                helperText={formik.touched.description && formik.errors.description}
+                inputProps={{
+                  maxLength: 200,
+                }}
+              />
+            </Grid>
 
+            <Grid item xs={12} md={6}>
+              <TextField
+                fullWidth
+                label="Fecha de ingreso"
+                name="entryDate"
+                type="date"
+                value={formik.values.entryDate}
+                onChange={formik.handleChange}
+                error={formik.touched.entryDate && formik.errors.entryDate}
+                helperText={formik.touched.entryDate && formik.errors.entryDate}
+                InputLabelProps={{
+                  shrink: true,
+                }}
+              />
+            </Grid>
 
+            <Grid item xs={12} md={12}>
+              <Button fullWidth size="large" type="submit" variant="contained">
+                Guardar
+              </Button>
+            </Grid>
 
-        </>
-    );
+            {/* Botón para volver a la lista de inventario */}
+            <Grid item xs={12} md={12}>
+              <Button
+                fullWidth
+                size="large"
+                variant="outlined"
+                onClick={() => router.push('/inventory')}
+              >
+                Volver a la lista de inventario
+              </Button>
+            </Grid>
+          </Grid>
+        </Box>
+      </Container>
+    </>
+  );
 }
