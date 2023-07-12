3664090268137bb821bb2522cef34436f555bd96
Ricardo1697 <rvr161997@gmail.com>
2023-07-11 19:02:53 -0600

    Fixes al inventory (listo inventory) y al sidebar

--- src/components/tables/inventory/InventoryList.js
+++ src/components/tables/inventory/InventoryList.js
@@ -13,7 +13,13 @@ import {
   TextField,
   Button,
   TablePagination,
+  InputAdornment,
 } from '@mui/material';
+import AddIcon from '@mui/icons-material/Add';
+import ManageSearchIcon from '@mui/icons-material/ManageSearch';
+import EditIcon from '@mui/icons-material/Edit';
+import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
+import InfoIcon from '@mui/icons-material/Info';
 import Link from 'next/link';
 import DeleteConfirmationDialog from '../../delete-dialog/DeleteDialog';
 import { AuthContext } from '../../../auth/JwtContext';
@@ -23,11 +29,31 @@ export default function InventoryList() {
   const [openDeleteDialog, setOpenDeleteDialog] = useState(false);
   const [deleteItemId, setDeleteItemId] = useState(null);
   const { accessToken } = useContext(AuthContext);
+  const [rows, setRows] = useState([]);
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(5);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleSearch = (event) => {
+    setSearchTerm(event.target.value.toLowerCase());
+    setPage(0);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
 
   useEffect(() => {
     fetchInventory();
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
+
+  const filteredRows = rows.filter((row) => row.productName.toLowerCase().includes(searchTerm));
 
   const fetchInventory = async () => {
     try {
@@ -39,7 +65,7 @@ export default function InventoryList() {
           },
         }
       );
-      setInventory(response.data);
+      setRows(response.data);
     } catch (error) {
       console.log(error);
     }
@@ -57,14 +83,11 @@ export default function InventoryList() {
 
   const handleDelete = async (id) => {
     try {
-      await axios.delete(
-        `https://control-financiero.herokuapp.com/api/v1/inventory/${id}`,
-        {
-          headers: {
-            Authorization: `Bearer ${accessToken}`,
-          },
-        }
-      );
+      await axios.delete(`https://control-financiero.herokuapp.com/api/v1/inventory/${id}`, {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      });
       setInventory((prevInventory) =>
         prevInventory.filter((inventoryItem) => inventoryItem.id !== id)
       );
@@ -89,11 +112,26 @@ export default function InventoryList() {
           sx={{ mb: 3 }}
           variant="contained"
           style={{ float: 'right' }}
+          startIcon={<AddIcon />}
         >
           Crear Inventario
         </Button>
 
-        <TextField fullWidth label="Buscar" name="search" sx={{ mb: 3 }} />
+        <TextField
+          fullWidth
+          label="Buscar"
+          name="search"
+          value={searchTerm}
+          onChange={handleSearch}
+          sx={{ mb: 3 }}
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">
+                <ManageSearchIcon />
+              </InputAdornment>
+            ),
+          }}
+        />
 
         <TableContainer component={Paper}>
           <Table>
@@ -103,10 +141,13 @@ export default function InventoryList() {
                 <TableCell>Cantidad disponible</TableCell>
                 <TableCell>Descripción</TableCell>
                 <TableCell>Fecha de ingreso</TableCell>
+                <TableCell>Acciones</TableCell>
               </TableRow>
             </TableHead>
             <TableBody>
-              {inventory.map((inventoryItem) => (
+            {filteredRows
+                .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                .map((inventoryItem) => (
                 <TableRow key={inventoryItem.id}>
                   <TableCell>{inventoryItem.productName}</TableCell>
                   <TableCell>{inventoryItem.availableQuantity}</TableCell>
@@ -115,43 +156,56 @@ export default function InventoryList() {
                   <TableCell>
                     <div>
                       <Button
-                        color="secondary"
+                        style={{ backgroundColor: 'orange' }}
                         component={Link}
                         href={`/inventory/edit/${inventoryItem.id}`}
                         size="small"
                         sx={{ mb: 2 }}
                         variant="contained"
+                        startIcon={<EditIcon />}
                       >
                         Editar
                       </Button>
                     </div>
                     <div>
                       <Button
-                        color="primary"
+                        color="error"
                         size="small"
                         sx={{ mb: 2 }}
                         variant="contained"
                         onClick={() => handleDeleteDialogOpen(inventoryItem.id)}
+                        startIcon={<DeleteForeverIcon />}
                       >
                         Eliminar
                       </Button>
-
+                    </div>
+                    <div>
                       <Button
-                          color="error"
-                          size="small"
-                          sx={{ mb: 2, mr: 2 }}
-                          variant="contained"
-                          component={Link}
-                          href={`/inventory/details/${inventoryItem.id}`}
-                        >
-                          Ver Detalles
-                        </Button>
+                        color="primary"
+                        size="small"
+                        sx={{ mb: 2, mr: 2 }}
+                        variant="contained"
+                        component={Link}
+                        href={`/inventory/details/${inventoryItem.id}`}
+                        startIcon={<InfoIcon />}
+                      >
+                        Ver Detalles
+                      </Button>
                     </div>
                   </TableCell>
                 </TableRow>
               ))}
             </TableBody>
           </Table>
+          <TablePagination
+            rowsPerPageOptions={[5, 10, 25]}
+            component="div"
+            count={filteredRows.length}
+            rowsPerPage={rowsPerPage}
+            page={page}
+            onPageChange={handleChangePage}
+            onRowsPerPageChange={handleChangeRowsPerPage}
+          />
         </TableContainer>
       </Container>
 
