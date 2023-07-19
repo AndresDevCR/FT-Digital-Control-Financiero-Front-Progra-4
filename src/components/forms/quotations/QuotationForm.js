644925c1923daedf4c4a6d0aeb089c3a31636019
Nalle12 <nallelyalfaro02@gmail.com>
2023-07-19 14:59:10 -0700

    cambios

---
+++ src/components/forms/quotations/QuotationForm.js
@@ -0,0 +1,313 @@
+import React, { useContext, useEffect, useState } from 'react';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
+import { Container, Typography, Box, Grid, TextField, Button, FormControl, InputLabel, Select, MenuItem } from '@mui/material';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import SaveIcon from '@mui/icons-material/Save';
+import KeyboardBackspaceIcon from '@mui/icons-material/KeyboardBackspace';
+import axios from 'axios';
+import { useRouter } from 'next/router';
+import { AuthContext } from '../../../auth/JwtContext';
+
+
+const validationSchema = Yup.object().shape({
+    client_id: Yup.number()
+        .required('Id de cliente es requerido')
+        .min(1, 'El campo debe tener como mínimo 1 dígito'),
+    total_payment: Yup.number()
+        .required('Total de pago es requerido')
+        .min(1, 'El campo debe tener como mínimo 1 dígito'),
+    total_payment_dollar: Yup.number()
+        .required('Total de pago en dólares es requerido')
+        .min(1, 'El campo debe tener como mínimo 1 dígito'),
+    e_invoice_code: Yup.string()
+        .required('Código de factura es requerido')
+        .min(1, 'El campo debe tener como mínimo 1 caracter'),
+    issue_date: Yup.date()
+        .required('Fecha de emisión es requerido'),
+    po_number: Yup.number()
+        .required('Número de orden de compra es requerido')
+        .min(1, 'El campo debe tener como mínimo 1 dígito'),
+    po_date: Yup.date()
+        .required('Fecha de orden de compra es requerido'),
+    description: Yup.string()
+        .required('Descripción es requerido')
+        .max(200, 'El campo debe tener como máximo 200 caracteres'),
+    quote_title: Yup.string()
+        .required('Título de cotización es requerido')
+        .max(30, 'El campo debe tener como máximo 30 caracteres'),
+
+});
+
+export default function QuotationForm() {
+    const { accessToken } = useContext(AuthContext);
+    const router = useRouter();
+    const [clients, setClients] = useState([]);
+
+    useEffect(() => {
+        const fetchClients = async () => {
+            try {
+                const response = await axios.get(
+                    'https://control-financiero.herokuapp.com/api/v1/client',
+                    {
+                        headers: {
+                            Authorization: `Bearer ${accessToken}`,
+                        },
+                    }
+                );
+                setClients(response.data);
+            } catch (error) {
+                console.error('Error fetching clients:', error);
+            }
+        };
+        fetchClients();
+    }, [accessToken]);
+
+    const handleSubmit = async (values) => {
+        try {
+            await axios.post('https://control-financiero.herokuapp.com/api/v1/quotation', values, {
+                headers: {
+                    Authorization: `Bearer ${accessToken}`, // Incluye el token de autenticación en el encabezado
+                },
+            });
+            toast.success('Agregado correctamente al inventario');
+            router.push('/quotation'); // Redireccionar a la lista de inventario
+        } catch (error) {
+            toast.error('Error al agregar al inventario');
+        }
+    };
+    
+    const formik = useFormik({
+        initialValues: {
+            client_id: 1,
+            total_payment: '',
+            total_payment_dollar: '',
+            e_invoice_code: '',
+            issue_date: '',
+            po_number: '',
+            po_date: '',
+            description: '',
+            quote_title: '',
+        },
+        validationSchema,
+        onSubmit: handleSubmit,
+    });
+
+    const handleInputChange = (event) => {
+        if (event.target.name === 'client_id' && !/^\d+$/.test(event.target.value)) {
+            toast.error('Solo se permiten números en el campo de Id de cliente');
+        }
+
+        if (event.target.name === 'availableQuantity' && !/^\d+$/.test(event.target.value)) {
+            toast.error('Solo se permiten números en el campo de Cantidad disponible');
+        }
+
+        if (event.target.name === 'availableQuantity' && event.target.value.length >= 7) {
+            event.target.value = event.target.value.slice(0, 6); // Limitar a 6 dígitos
+            toast.info('Se ha alcanzado el límite de números permitidos ');
+        }
+
+        if (event.target.name === 'description' && event.target.value.length >= 200) {
+            toast.info('Se ha alcanzado el límite de caracteres permitidos para la descripción');
+        }
+
+        formik.handleChange(event);
+    };
+
+    return (
+        <>
+            <Container>
+                <Typography variant="h3" component="h1" paragraph>
+                    Agregar Cotización
+                </Typography>
+            </Container>
+
+            <ToastContainer />
+
+            <Container>
+                <Box
+                    component="form"
+                    noValidate
+                    autoComplete="off"
+                    sx={{ mt: 3 }}
+                    onSubmit={formik.handleSubmit}
+                >
+
+                    <Grid item xs={12} md={12}>
+                        <FormControl fullWidth>
+                            <InputLabel id="client-label">Cliente</InputLabel>
+                            <Select
+                                labelId="client-label"
+                                id="client_id"
+                                type='number'
+                                name="client_id"
+                                value={formik.values.client_id}
+                                onChange={formik.handleChange}
+                                error={formik.touched.client_id && formik.errors.client_id}
+                                fullWidth
+                            >
+                                {clients.map((client) => (
+                                    <MenuItem key={client.id} value={client.id}>
+                                        {client.client_name}
+                                    </MenuItem>
+                                ))}
+                            </Select>
+                        </FormControl>
+                    </Grid>
+                    <br />
+
+
+                    <Grid container spacing={3}>
+                        <Grid item xs={12} md={6}>
+                            <TextField
+                                fullWidth
+                                label="Total a pagar"
+                                name="total_payment"
+                                type="number"
+                                value={formik.values.total_payment}
+                                onChange={handleInputChange}
+                                error={formik.touched.total_payment && formik.errors.total_payment}
+                                helperText={formik.touched.total_payment && formik.errors.total_payment}
+                                inputProps={{
+                                    max: 999999,
+                                    maxLength: 6,
+                                }}
+
+                            />
+                        </Grid>
+
+                        <Grid item xs={12} md={6}>
+                            <TextField
+                                fullWidth
+                                label="Total a pagar en dolares"
+                                name="total_payment_dollar"
+                                type="number"
+                                value={formik.values.total_payment_dollar}
+                                onChange={handleInputChange}
+                                error={formik.touched.total_payment_dollar && formik.errors.total_payment_dollar}
+                                helperText={formik.touched.total_payment_dollar && formik.errors.total_payment_dollar}
+                                inputProps={{
+                                    max: 999999,
+                                    maxLength: 6,
+                                }}
+                            />
+
+
+                        </Grid>
+
+                        <Grid item xs={12} md={12}>
+                            <TextField
+                                fullWidth
+                                label="Codigo de factura electronica"
+                                name="e_invoice_code"
+                                type="text"
+                                value={formik.values.e_invoice_code}
+                                onChange={handleInputChange}
+                                error={formik.touched.e_invoice_code && formik.errors.e_invoice_code}
+                                helperText={formik.touched.e_invoice_code && formik.errors.e_invoice_code}
+                                inputProps={{
+                                    maxLength: 30,
+                                }}
+                            />
+                        </Grid>
+
+                        <Grid item xs={12} md={6}>
+                            <TextField
+                                fullWidth
+                                label="Fecha de emision"
+                                name="issue_date"
+                                type="date"
+                                value={formik.values.issue_date}
+                                onChange={handleInputChange}
+                                error={formik.touched.issue_date && formik.errors.issue_date}
+                                helperText={formik.touched.issue_date && formik.errors.issue_date}
+                            />
+                        </Grid>
+
+                        <Grid item xs={12} md={6}>
+                            <TextField
+                                fullWidth
+                                label="Numero de orden de compra"
+                                name="po_number"
+                                type="number"
+                                value={formik.values.po_number}
+                                onChange={handleInputChange}
+                                error={formik.touched.po_number && formik.errors.po_number}
+                                helperText={formik.touched.po_number && formik.errors.po_number}
+                                inputProps={{
+                                    max: 999999,
+                                    maxLength: 6,
+                                }}
+                            />
+                        </Grid>
+
+                        <Grid item xs={12} md={6}>
+                            <TextField
+                                fullWidth
+                                label="Fecha de orden de compra"
+                                name="po_date"
+                                type="date"
+                                value={formik.values.po_date}
+                                onChange={handleInputChange}
+                                error={formik.touched.po_date && formik.errors.po_date}
+                                helperText={formik.touched.po_date && formik.errors.po_date}
+                            />
+
+                        </Grid>
+                        <Grid item xs={12} md={6}>
+                            <TextField
+                                fullWidth
+                                label="Descripción"
+                                name="description"
+                                type="text"
+                                value={formik.values.description}
+                                onChange={handleInputChange}
+                                error={formik.touched.description && formik.errors.description}
+                                helperText={formik.touched.description && formik.errors.description}
+                                inputProps={{
+                                    maxLength: 200,
+                                }}
+                            />
+                        </Grid>
+
+                        <Grid item xs={12} md={6}>
+                            <TextField
+                                fullWidth
+                                label="Título de cotización"
+                                name="quote_title"
+                                type="text"
+                                value={formik.values.quote_title}
+                                onChange={handleInputChange}
+                                error={formik.touched.quote_title && formik.errors.quote_title}
+                                helperText={formik.touched.quote_title && formik.errors.quote_title}
+                                inputProps={{
+                                    maxLength: 30,
+                                }}
+                            />
+                        </Grid>
+
+                        <Grid item xs={12} md={12}>
+                            <Button fullWidth size="large" type="submit" variant="contained" startIcon={<SaveIcon />}>
+                                Guardar
+                            </Button>
+                        </Grid>
+
+
+                        <Grid item xs={12} md={12}>
+                            <Button
+                                fullWidth
+                                size="large"
+                                variant="outlined"
+                                onClick={() => router.push('/quotation')}
+                                startIcon={<KeyboardBackspaceIcon />}
+                            >
+                                Volver a la lista de cotizaciones
+                            </Button>
+                        </Grid>
+                    </Grid>
+                </Box>
+            </Container>
+        </>
+    );
+};
---
+++ src/pages/dashboard/company/edit/[id]/index.js
@@ -0,0 +1,304 @@
+/* eslint-disable no-unused-vars */
+import React, { useState, useEffect, useContext } from 'react';
+import { useRouter } from 'next/router';
+import axios from 'axios';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import Head from 'next/head';
+import { Container, Typography, Box, Grid, TextField, Button, FormControlLabel, Checkbox } from '@mui/material';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
+
+import DashboardLayout from '../../../../../layouts/dashboard';
+import { useSettingsContext } from '../../../../../components/settings';
+import { AuthContext } from '../../../../../auth/JwtContext';
+
+const validationSchema = Yup.object().shape({
+  name: Yup.string()
+    .required('Nombre de la compañía es requerido')
+    .max(30, 'El nombre debe tener como máximo 30 caracteres'),
+  description: Yup.string()
+    .required('Descripción de la compañía es requerida')
+    .max(200, 'La descripción debe tener como máximo 200 caracteres'),
+  category: Yup.string()
+    .required('Categoría de la compañía es requerida')
+    .max(30, 'La categoría debe tener como máximo 30 caracteres'),
+  primary_phone_number: Yup.string()
+    .required('Número de teléfono es requerido')
+    .max(30, 'El número de teléfono debe tener como máximo 30 caracteres'),
+  secondary_phone_number: Yup.string()
+    .required('Número de teléfono es requerido')
+    .max(30, 'El número de teléfono debe tener como máximo 30 caracteres'),
+  city: Yup.string()
+    .required('Ciudad es requerida')
+    .max(30, 'La ciudad debe tener como máximo 30 caracteres'),
+  state: Yup.string()
+    .required('Estado es requerido')
+    .max(30, 'El estado debe tener como máximo 30 caracteres'),
+  country: Yup.string()
+    .required('País es requerido')
+    .max(30, 'El país debe tener como máximo 30 caracteres'),
+  is_active: Yup.boolean().required('Estado es requerido').default(true),
+});
+
+const EditCompany = () => {
+  const { accessToken } = useContext(AuthContext);
+  const { themeStretch } = useSettingsContext();
+  const router = useRouter();
+  const { id } = router.query;
+
+  const handleSubmit = (values) => {
+    axios
+      .patch(`https://control-financiero.herokuapp.com/api/v1/company/${id}`, values, {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        }
+      })
+      .then((response) => {
+        console.log(response);
+        toast.success('Compañía actualizada con éxito');
+        router.push('/dashboard/company/list');
+      })
+      .catch((error) => {
+        console.log(error);
+        toast.error('Error al actualizar la compañía');
+      });
+  };
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+
+    if (value.length >= 30) {
+      toast.info('Se ha alcanzado el límite de caracteres permitidos');
+    }
+
+    formik.setFieldValue(name, value);
+  };
+
+  const formik = useFormik({
+    initialValues: {
+      name: '',
+      display_name: '',
+      description: '',
+      is_active: true,
+    },
+    validationSchema,
+    onSubmit: handleSubmit,
+  });
+
+  useEffect(() => {
+    if (id) {
+      // Fetch company data from the API using the provided ID
+      axios
+        .get(`https://control-financiero.herokuapp.com/api/v1/company/${id}`, {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
+        })
+        .then((response) => {
+          const company = response.data;
+          formik.setValues({
+            name: company.name,
+            description: company.description,
+            category: company.category,
+            primary_phone_number: company.primary_phone_number,
+            secondary_phone_number: company.secondary_phone_number,
+            city: company.city,
+            state: company.state,
+            country: company.country,
+            is_active: company.is_active,
+          });
+        })
+        .catch((error) => {
+          console.log(error);
+          toast.error('Error al cargar el inventario');
+        });
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [accessToken, id, formik.setValues]);
+
+  return (
+    <>
+      <Head>
+        <title>Editar compañía | FT Control Financiero</title>
+      </Head>
+
+      <Container maxWidth={themeStretch ? false : 'xl'}>
+        <Typography variant="h3" component="h1" paragraph>
+          Editar compañía
+        </Typography>
+      </Container>
+
+      <ToastContainer />
+
+      {/* Formulario de editar compañía */}
+      <Container maxWidth={themeStretch ? false : 'xl'}>
+        <Box
+          component="form"
+          noValidate
+          autoComplete="off"
+          sx={{ mt: 3 }}
+          onSubmit={formik.handleSubmit}
+        >
+          <Grid container spacing={3}>
+            <Grid item xs={12} md={12}>
+              <TextField
+                fullWidth
+                label="Nombre de la compañía"
+                name="name"
+                value={formik.values.name}
+                onChange={handleInputChange}
+                error={formik.touched.name && formik.errors.name}
+                helperText={formik.touched.name && formik.errors.name}
+                inputProps={{
+                  maxLength: 30,
+                }}
+              />
+            </Grid>
+
+            <Grid item xs={12} md={12}>
+              <TextField
+                fullWidth
+                label="Descripción de la Compañía"
+                name="description"
+                value={formik.values.description}
+                onChange={handleInputChange}
+                error={formik.touched.description && formik.errors.description}
+                helperText={formik.touched.description && formik.errors.description}
+                inputProps={{
+                  maxLength: 200,
+                }}
+              />
+            </Grid>
+
+            <Grid item xs={12} md={12}>
+              <TextField
+                fullWidth
+                label="Categoría"
+                name="category"
+                value={formik.values.category}
+                onChange={handleInputChange}
+                error={formik.touched.category && formik.errors.category}
+                helperText={formik.touched.category && formik.errors.category}
+                inputProps={{
+                  maxLength: 30,
+                }}
+              />
+            </Grid>
+
+            <Grid item xs={12} md={12}>
+              <TextField
+                fullWidth
+                label="Primer número de teléfono"
+                name="primary_phone_number"
+                value={formik.values.primary_phone_number}
+                onChange={handleInputChange}
+                error={formik.touched.primary_phone_number && formik.errors.primary_phone_number}
+                helperText={formik.touched.primary_phone_number && formik.errors.primary_phone_number}
+                inputProps={{
+                  maxLength: 30,
+                }}
+              />
+            </Grid>
+
+            <Grid item xs={12} md={12}>
+              <TextField
+                fullWidth
+                label="Segundo número de teléfono"
+                name="secondary_phone_number"
+                value={formik.values.secondary_phone_number}
+                onChange={handleInputChange}
+                error={formik.touched.secondary_phone_number && formik.errors.secondary_phone_number}
+                helperText={formik.touched.secondary_phone_number && formik.errors.secondary_phone_number}
+                inputProps={{
+                  maxLength: 30,
+                }}
+              />
+            </Grid>
+
+            <Grid item xs={12} md={12}>
+              <TextField
+                fullWidth
+                label="Ciudad"
+                name="city"
+                value={formik.values.city}
+                onChange={handleInputChange}
+                error={formik.touched.city && formik.errors.city}
+                helperText={formik.touched.city && formik.errors.city}
+                inputProps={{
+                  maxLength: 30,
+                }}
+              />
+            </Grid>
+
+            <Grid item xs={12} md={12}>
+              <TextField
+                fullWidth
+                label="Estado"
+                name="state"
+                value={formik.values.state}
+                onChange={handleInputChange}
+                error={formik.touched.state && formik.errors.state}
+                helperText={formik.touched.state && formik.errors.state}
+                inputProps={{
+                  maxLength: 30,
+                }}
+              />
+            </Grid>
+
+            <Grid item xs={12} md={12}>
+              <TextField
+                fullWidth
+                label="País"
+                name="country"
+                value={formik.values.country}
+                onChange={handleInputChange}
+                error={formik.touched.country && formik.errors.country}
+                helperText={formik.touched.country && formik.errors.country}
+                inputProps={{
+                  maxLength: 30,
+                }}
+              />
+            </Grid>
+
+            <Grid item xs={12} md={6}>
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    checked={formik.values.is_active}
+                    onChange={handleInputChange}
+                    name="is_active"
+                    color="primary"
+                  />
+                }
+                label="Activo"
+              />
+            </Grid>
+
+            <Grid item xs={12} md={12}>
+              <Button fullWidth size="large" type="submit" variant="contained">
+                Guardar
+              </Button>
+            </Grid>
+
+            {/* Botón para volver a la lista de compañías */}
+            <Grid item xs={12} md={12}>
+              <Button
+                fullWidth
+                size="large"
+                variant="outlined"
+                onClick={() => router.push("/dashboard/company/list")}
+              >
+                Volver a la lista de compañías
+              </Button>
+            </Grid>
+          </Grid>
+        </Box>
+      </Container>
+    </>
+  );
+};
+
+EditCompany.getLayout = (page) => <DashboardLayout>{page}</DashboardLayout>;
+
+export default EditCompany;
